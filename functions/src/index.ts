7f5d22bfcca831eca20ebea03019a5aadead2db7
TomariRyusei <t.ryu6635@gmail.com>
2023-04-10 22:30:02 +0900

    first commit

---
+++ functions/src/index.ts
@@ -0,0 +1,7 @@
+import * as functions from "firebase-functions";
+import { myMangaList } from "./myMangaList";
+
+export const scrapeManga = functions.https.onRequest((request, response) => {
+  functions.logger.info(myMangaList[0], { structuredData: true });
+  response.send(myMangaList[0]);
+});
