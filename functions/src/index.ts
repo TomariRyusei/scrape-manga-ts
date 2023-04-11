a8b93ec23a7123e5e50036e3e01d271342448b01
TomariRyusei <t.ryu6635@gmail.com>
2023-04-11 19:22:27 +0900

    Change response value

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,5 +3,5 @@ import { myMangaList } from "./myMangaList";
 
 export const scrapeManga = functions.https.onRequest((request, response) => {
   functions.logger.info(myMangaList[0], { structuredData: true });
-  response.send(myMangaList[0]);
+  response.send(myMangaList[1]);
 });
