5f81871d7d5c3b13281ba1fb5fa0332e503ae431
TomariRyusei <t.ryu6635@gmail.com>
2023-04-11 21:44:34 +0900

    Add scraping code sample

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,22 @@
 import * as functions from "firebase-functions";
-import { myMangaList } from "./myMangaList";
+import puppeteer from "puppeteer";
 
-export const scrapeManga = functions.https.onRequest((request, response) => {
-  functions.logger.info(myMangaList[0], { structuredData: true });
-  response.send(myMangaList[1]);
-});
+// import { myMangaList } from "./myMangaList";
+
+const scraper = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://www.navi-comi.com/20488/arrival-list/");
+  await page.screenshot({
+    path: "/Users/tryu/Desktop/kaikatsu_arrival-list.png",
+  });
+  await browser.close();
+};
+export const scrapeManga = functions.https.onRequest(
+  async (request, response) => {
+    functions.logger.info("Scraping started!", { structuredData: true });
+    await scraper();
+    functions.logger.info("Scraping completed!", { structuredData: true });
+    response.send("OK!");
+  }
+);
