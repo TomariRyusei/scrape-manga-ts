e63ae5b98cc26e17caadbea02e6ec5f6f99d20c8
TomariRyusei <t.ryu6635@gmail.com>
2023-11-19 15:31:06 +0900

    Move subscription list for test into test code

--- functions/__tests__/filterNewArrivalList.test.ts
+++ functions/__tests__/filterNewArrivalList.test.ts
@@ -3,7 +3,22 @@ import {
   NewArrival,
 } from "../src/index";
 
-import { mySubscriptionListForTest } from "./mySubscriptionListForTest";
+const mySubscriptionListForTest = [
+  "健康で文化的な最低限度の生活",
+  "九条の大罪",
+  "BORUTO",
+  "彼岸島",
+  "GANTZ:E",
+  "ダーウィン事変",
+  "JUMBO MAX",
+  "GIGANTIS－ジャイガンティス－",
+  "ヒソカニアサレ",
+  "銭麻",
+  "ケーキの切れない非行少年たち",
+  "呪術廻戦",
+  "イリオス",
+  "アンダーニンジャ",
+];
 
 describe("filterNewArrivalList", () => {
   const arrivalList: NewArrival[] = [
